414c7598607bbdfdaf3f70c55ef894df8ef12bf7
dasoib <dasojib49@gmail.comm>
2023-05-05 11:25:51 +0600

    chef data done

--- index.js
+++ index.js
@@ -4,6 +4,7 @@ const cors = require('cors');
 const port = process.env.PORT || 5000;
 
 const categories = require('./data/categories.json');
+const chef = require('./data/chef.json')
 
 app.use(cors())
 
@@ -13,6 +14,23 @@ app.get('/',(req, res)=>{
 
 app.get('/categories',(req,res) =>{
           res.send(categories);
+})
+
+app.get('/chef',(req,res)=>{
+          res.send(chef);
+})
+
+app.get('/chef/:id',(req,res)=>{
+          const id = req.params.id;
+          console.log(id)
+          const selectedChef = chef.find(c =>c.id === id)
+          res.send(selectedChef);
+})
+
+app.get('/categories/:id',(req,res) =>{
+          const id = req.params.id;
+          console.log(id)
+          
 })
 
 app.listen(port, ()=>{
